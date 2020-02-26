92cc68e3017ddffb6ebe0a459bdcae2b4a79dbc6
catw0man <61030815+catw0man@users.noreply.github.com>
2020-02-25 21:04:20 -0300

    Add files via upload

---
+++ src/tablerodos.java
@@ -0,0 +1,81 @@
+package Oscurilandia;
+
+import java.util.Scanner;
+public class tablerodos {
+	public void mostrartablero1(){
+	int largo=16;
+	int ancho=18;
+	int i;
+	int p;
+	
+	int tablero[][] = new int[largo][ancho];{
+			System.out.println("¡HOlA!, elije una posición para lanzar tu huevo");
+			for (i=1;i<largo;i++) {			
+				   
+				if ((i==1) && (i<16)){
+				
+					System.out.println
+					
+					("    A   B   C   D   E   F   G   H   I   J   K   L   M   N   O");
+    				
+				}
+				
+				System.out.println(" ______________________________________________________________");
+				
+				
+				for (p=1;p<ancho;p++) {				
+						
+						if (p==18) {
+				
+							System.out.print("|   |");					
+						}
+					
+						else if((p==1) && (i<10)){
+						
+							System.out.print(" "+i);
+						}
+						
+						else if((p==1) && (i>9 && i<16)){
+						    System.out.print(i);
+						}
+						else {
+						
+							System.out.print(" |  ");
+						}
+							
+					}
+					
+					System.out.println("");
+				}
+				System.out.println(" ______________________________________________________________"); 
+				
+	}			
+
+	Kromi[]kromis= new Kromi[3];
+		kromis[0]= 3,"kfldsñ",2,3,323,"ds";
+	}
+		
+		int[][]K =new int[15][15]; 
+		
+		for(int f = 0; f<15;f++) 
+		{
+			for(int j=0;j<15;j++)
+			{
+				K[f][j]=0; 
+			}
+		}	}
+}
+
+	
+
+
+	
+	
+	
+
+ 
+
+
+
+
+
