683c30b14b3446b64f896d56e3febf9a88a266b7
catw0man <61030815+catw0man@users.noreply.github.com>
2020-02-25 21:02:02 -0300

    avance 25/2
    poquito pero seguro:(

---
+++ src/Ejecutable.java
@@ -0,0 +1,16 @@
+
+package Oscurilandia;
+
+	public class Ejecutable {
+
+		public static void main(String[] args) {
+			
+			
+			tablerodos t = new tablerodos();
+			t.mostrartablero1();
+			
+
+		}
+
+	
+}
